fbe811a85805c787147c0c9413a7bb3f4979fbed
OlegPlugaru <olegplugaru.md@gmail.com>
2024-02-28 21:06:33 +0200

    Initial Commit

---
+++ DatabaseTesting/DatabaseTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatabaseTestingApp.swift
+//  DatabaseTesting
+//
+//  Created by Oleg Plugaru on 28.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct DatabaseTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
